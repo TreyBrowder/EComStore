7e65863aef6473130262898bcfda36e34b38518d
Treybrowder <tbrowder7@gmail.com>
2024-10-31 18:16:16 -0400

    First Commit

---
+++ EcomStore/App/EcomStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcomStoreApp.swift
+//  EcomStore
+//
+//  Created by Trey Browder on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct EcomStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
